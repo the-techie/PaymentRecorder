45e730964d5ad41253ad4d0b4f8eb53f7bb155e9
the-techie <dhirender.programmer@gmail.com>
2022-07-20 19:13:57 +0530

    Final file

--- PaymentRecorder.py
+++ PaymentRecorder.py
@@ -8,7 +8,8 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import base64
 from bs4 import BeautifulSoup
-
+from pymongo import MongoClient
+import time
 
 class PaymentRecorder:
     def getMessageIds(self):
@@ -16,10 +17,33 @@ class PaymentRecorder:
         c = 0
         threads = []
 
+        count = self.db["last_access"].count_documents({"_id": 1})
+        last_access = int(time.time())
+
+        q = "from: alerts@axisbank.com"
+
+        if(count == 0):
+            # insert current at last accessed
+            db = self.db["last_access"].insert_one({"_id": 1, "last_time": int(time.time())})
+        else:
+            res = self.db["last_access"].find_one({})
+            # print(res)
+            last_access = res["last_time"]
+            q = q + " after: " + str(last_access)
+            self.db["last_access"].update_one({"_id": 1}, { "$set" : {"last_time": int(time.time())} })
+
+        # q = q + str(last_access)
+        print("q:\t" + q)
         while(c ==0 or nextPage):
             c += 1
-            results = self.service.users().messages().list(pageToken = nextPage, q = "from: alerts@axisbank.com", userId = "me").execute()
-            messages = results["messages"]
+            results = self.service.users().messages().list(pageToken = nextPage, q = q, userId = "me").execute()
+            # print("results:\t", results)
+            messages = results.get("messages", None)
+
+            if messages is None:
+                print("No new mails")
+                return None
+
             for i in messages:
                 threads.append(i["id"])
             nextPage = results.get("nextPageToken", None)
@@ -46,7 +70,16 @@ class PaymentRecorder:
     def getCreditInfo(self, string):
         infos = []
 
-        msg = "INR {} credited to A/c no. {} on {} at {}. Info- {}Avl Bal- INR {} - Axis Bank"
+
+        msg = "INR {} credited to A/c no. {} on {} at {} IST. Info- {}Avl Bal- INR {} - Axis Bank"
+        if "Info:" in string:
+            msg = "INR {} credited to A/c no. {} on {} at {} IST. Info: {}Avl Bal- INR {} - Axis Bank"
+
+        if string.startswith("Dear Customer,"):
+            msg = "Dear Customer, INR {} credited to A/c no. {} on {} at {} IST. Info- {}Avl Bal- INR {} - Axis Bank"
+
+            if "Info:" in string:
+                msg = "Dear Customer, INR {} credited to A/c no. {} on {} at {} IST. Info: {}Avl Bal: INR {} - Axis Bank"
 
         keywords = msg.split("{}")
 
@@ -56,17 +89,28 @@ class PaymentRecorder:
 
         for i in range(1, n):
             next = string.find(keywords[i])
+            if next == -1:
+                return
             info = ''.join(string[last : next])
             infos.append(info)
             last = next + len(keywords[i])
 
         # print("\n\n\n")
-        return infos
+        # return infos
+        print("============Credit=========")
+        print("\n".join(infos), "\n\n")
+        self.db["Credits"].insert_one({"_id" : infos[4], "amount": infos[0], "pay_time" : infos[2]+infos[3], "balance" : infos[5]})
 
     def getDebitInfo(self, string):
         infos = []
 
         msg = "Dear Customer, We wish to inform you that INR {} has been debited from your A/c no. {} on {} at {} Available balance: INR {} Please"
+
+        if not msg.startswith("Dear Customer,"):
+            msg = "INR {} debited to A/c no. {} on {} at {} IST. Info- {}Avl Bal- INR {} - Axis Bank"
+            
+
+
 
         keywords = msg.split("{}")
         # print(keywords)
@@ -82,7 +126,10 @@ class PaymentRecorder:
             last = next + len(keywords[i])
 
         # print("\n\n\n")
-        return infos
+        # return infos
+        print("============Debit=========")
+        print("\n".join(infos), "\n\n")
+        self.db["Debits"].insert_one({"_id" : infos[3], "amount": infos[0], "pay_time" : infos[2], "balance" : infos[4]})
 
     def __init__(self):
         SCOPES = ['https://mail.google.com/']
@@ -105,9 +152,18 @@ class PaymentRecorder:
         except HttpError as error:
             print(f'An error occurred: {error}')
 
+        # self.db = Database("PaymentsInfo")
+        data = json.load(open("mongo_info.json"))
+        self.client = MongoClient(data["url"])
+        self.db = self.client["PaymentsInfo"]
+
     def start(self):
         msgIds = self.getMessageIds()
-        for i in range(10):
+
+        if msgIds is None:
+            return
+
+        for i in range(len(msgIds)):
 
             msg = self.getMessage(msgIds[i])
             # print(msg)
@@ -115,14 +171,14 @@ class PaymentRecorder:
             
 
             if("credited" in msg):
-                print("\n\n===credited===")
+                # print("\n\n===credited===")
                 res = self.getCreditInfo(msg)
-                print("\n".join(res))
+                # print("\n".join(res))
 
             else:
-                print("\n\n===Debited===")
+                # print("\n\n===Debited===")
                 res = self.getDebitInfo(msg)
-                print("\n".join(res))
+                # print("\n".join(res))
 
 
 p1 = PaymentRecorder()
